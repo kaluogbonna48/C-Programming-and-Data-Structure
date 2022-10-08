4cf06d1ce6a0c35b290a9f29ec2f7dabec8ad439
Kalu Ogbonna <KALUOGBONNA48@GMAIL.COM>
2022-10-08 17:30:04 +0100

    Added C tutorial code for scanf function.c
    1. Scanf function takes in two arguments.
    2. First being the format specifier for an input from keyboard, 
    and the later being the address of the memory location to save the input.
    3. Scanf can accept int, char and string from the standard input (keyboard).

---
+++ scanf function in c.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("%s \n", "Please enter the first number:");
+    scanf("%d", &a);
+    printf("%s \n", "Please enter the first number:");
+    scanf("%d", &b);
+    printf("The sum of the two numbers is: %d", a + b);
+    return 0;
+}
