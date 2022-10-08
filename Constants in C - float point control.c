56abc58759134db65e1f375aa399c081102712e5
Kalu Ogbonna <KALUOGBONNA48@GMAIL.COM>
2022-10-08 14:16:44 +0100

    Add C tutorial code for float point control
    Decimal points in C programming can be controlled by adding the decimal place in between the format specifier.
    e.g printf("%.3f", 7.456924) will print 7.456

---
+++ Constants in C - float point control.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#define PI 3.14159
+
+int main() 
+{
+    // Write C code here
+    printf("The value of PI is: %.5f\n", PI);
+    return 0;
+}
