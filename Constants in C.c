5bf13aaf19b403ec242a7e2486d579dfedf05894
Kalu Ogbonna <KALUOGBONNA48@GMAIL.COM>
2022-10-08 13:06:40 +0100

    Add C tutorial code for constants
    Constants in c, when declared can not be changed.
    Constants can be declared with either, "const", or "#define"

---
+++ Constants in C.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#define add(x,y) x+y
+
+int main() {
+    // Write C code here
+    
+    printf("The expression of a * b + c is: %d", 5 * add(5,6));
+
+    return 0;
+}
