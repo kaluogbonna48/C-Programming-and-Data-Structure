02d0399be40dced1c5a8b7e300f9262cdf4f4798
Kalu Ogbonna <KALUOGBONNA48@GMAIL.COM>
2022-10-09 18:18:35 +0100

    Practice logical operators in C
     * && - combines condition and 
     * returns true if the two are true
     * && - Will shortcircuit 
     * when it encounters false condition.
     * || - combines condition and 
     * returns true if any of the two is true.

---
+++ Logical Operators in C.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * Logical Operator
+ * && - combines condition and 
+ * returns true if the two are true
+ * && - Will shortcircuit 
+ * when it encounters false condition.
+ * || - combines condition and 
+ * returns true if any of the two is true. 
+ */
+ 
+int main() {
+    int a = 4, b = 5;
+    int incr;
+    //OR Operator
+    incr = (a > b) || (b++);
+    printf("%d\n", incr);
+    printf("%d\n", b);
+    //AND Operator
+    incr = (a > b) && (b++);
+    printf("%d\n", incr);
+    printf("%d\n", b);
+    
+    return 0;
+}
