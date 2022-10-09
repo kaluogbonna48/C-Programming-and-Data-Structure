c427545cce122b15cbbef0253af0defc1ed78bea
Kalu Ogbonna <KALUOGBONNA48@GMAIL.COM>
2022-10-09 09:06:20 +0100

    Create C code challenge - Converting Hex to Decimal.c
     * Converting Hex to Decimal
     * 0x - dec to hex form
     * "%x" - print lowercase hex
     *  "%X" - print Uppercase hex

---
+++ C code challenge - Converting Hex to Decimal.c
@@ -0,0 +1,16 @@
+/**
+ * Converting Hex to Decimal
+ * 0x - dec to hex form
+ * "%x" - print lowercase hex
+ *  "%X" - print Uppercase hex
+ */ 
+ 
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int var = 0x43FF;
+    printf("%X", var);
+
+    return 0;
+}
