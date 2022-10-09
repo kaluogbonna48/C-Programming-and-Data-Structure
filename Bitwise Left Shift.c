114791334f6d53a68b87a1a4d54a5de6f4ada536
Kalu Ogbonna <KALUOGBONNA48@GMAIL.COM>
2022-10-09 22:04:26 +0100

    Practise Bitwise Left Shift Operator
     * Bitwise Operator
     * << - Bitwise Left Shift)- shifts 
     * bits (binary equivalant) to left and 
     * returns the decimal equivalant of the binary formed.

---
+++ Bitwise Left Shift.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Bitwise Operator
+ * << - (Bitwise Left Shift)- shifts 
+ * bits (binary equivalant) to left and 
+ * returns the decimal equivalant of the binary formed.
+ */
+ 
+int main() {
+    char var = 5; 
+    printf("%d\n", var<<1);
+    printf("%d\n", var<<2);
+    printf("%d\n", var<<3);
+    printf("%d\n", var<<4);
+
+    return 0;
+}
